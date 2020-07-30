fd53c670101b56bb86901f26b6e2140fa89fcb44
caolecongminh <caolecongminh1997@gmail.com>
2020-07-30 09:45:01 +0700

    files was created by visual studio 2017

---
+++ Algorithms/Learning_ML.cpp
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////
+//                                             //
+//        Created by Cao Le Cong Minh          //
+//                                             //
+/////////////////////////////////////////////////
+//   Gmail:  caolecongminh1997@gmail.com       //
+//	Github: https://github.com/Minh-CaoLeCong  //
+/////////////////////////////////////////////////
+
+#ifndef Machine_Learning_Algorithms_h
+#include "Machine_Learning_Algorithms.h"
+#endif Machine_Learning_Algorithms_h
+
+#ifndef Matrix_Operation_h
+#include "Matrix_Operation.h"
+#endif Matrix_Operation_h
+
+using namespace std;
+
+int main()
+{
+
+	cout << "\nHello World!\n\n";
+	//Stochastic_Gradient_Descent();
+
+	/*int arr[][2] = { {1, 2104}, {1,1416}, {1, 2104}, {1,1416}, {1, 2104}, {1,1416}, {1, 2104}, {1,1416}, };
+	unsigned int n = sizeof(arr) / sizeof(arr[0][0]);
+	cout << sizeof(arr) << endl;
+	cout << sizeof(arr[0][0]) << endl;
+	cout << n << endl;*/
+
+	/*NormalEquation();*/
+
+	kNN_Example();
+}
+
+/////////////////////////////////////////////////
+//                                             //
+//        Created by Cao Le Cong Minh          //
+//                                             //
+/////////////////////////////////////////////////
+//   Gmail:  caolecongminh1997@gmail.com       //
+//	Github: https://github.com/Minh-CaoLeCong  //
+/////////////////////////////////////////////////
